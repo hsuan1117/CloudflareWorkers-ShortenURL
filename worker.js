820da248990ecfb774fd1b1c8b6ac03fffc9d8f1
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2021-06-29 09:18:41 +0800

    update worker

--- worker.js
+++ worker.js
@@ -1,81 +1,7 @@
 /*Credit: https://github.com/xyTom/Url-Shorten-Worker*/
-const html404 = `<!DOCTYPE html>
-<body>
-  <h1>404 Not Found.</h1>
-  <p>The url you visit is not found.</p>
-</body>`
-
-
-async function randomString(len) {
-    len = len || 6;
-    let $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';
-    /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
-    let maxPos = $chars.length;
-    let result = '';
-    for (i = 0; i < len; i++) {
-        result += $chars.charAt(Math.floor(Math.random() * maxPos));
-    }
-    return result;
-}
-
-async function checkURL(URL) {
-    let str = URL;
-    let Expression = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/;
-    let objExp = new RegExp(Expression);
-    if (objExp.test(str) === true) {
-        return str[0] === 'h';
-    } else {
-        return false;
-    }
-}
-
-async function save_url(URL, key) {
-    let random_key = key;
-    if(key === ''){
-        random_key = await randomString()
-    }
-    let is_exist = await LINKS.get(random_key)
-    console.log(is_exist)
-    if (is_exist == null)
-        return await LINKS.put(random_key, URL), random_key
-    else
-        save_url(URL,'')
-}
 
 async function handleRequest(request) {
-    console.log(request)
-    if (request.method === "POST") {
-        let req = await request.json()
-        console.log(req["url"])
-        if (!await checkURL(req["url"])) {
-            return new Response(`{"status":500,"key":": Error: Url illegal."}`, {
-                headers: {
-                    "content-type": "text/html;charset=UTF-8",
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods": "POST",
-                },
-            })
-        }
-        let stat, random_key = await save_url(req["url"], req["key"])
-        console.log(stat)
-        if (typeof (stat) == "undefined") {
-            return new Response(`{"status":200,"key":"/` + random_key + `"}`, {
-                headers: {
-                    "content-type": "text/html;charset=UTF-8",
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods": "POST",
-                },
-            })
-        } else {
-            return new Response(`{"status":200,"key":": Error:Reach the KV write limitation."}`, {
-                headers: {
-                    "content-type": "text/html;charset=UTF-8",
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods": "POST",
-                },
-            })
-        }
-    } else if (request.method === "OPTIONS") {
+    if (request.method === "OPTIONS") {
         return new Response(``, {
             headers: {
                 "content-type": "text/html;charset=UTF-8",
@@ -83,38 +9,19 @@ async function handleRequest(request) {
                 "Access-Control-Allow-Methods": "POST",
             },
         })
-
     }
 
     const requestURL = new URL(request.url)
     const path = requestURL.pathname.split("/")[1]
-    console.log(path)
+
     if (!path) {
-
-        const html = await fetch("https://cdn.jsdelivr.net/gh/hsuan1117/CloudflareWorkers-ShortenURL@main/index.html")
-
-        return new Response(await html.text(), {
-            headers: {
-                "content-type": "text/html;charset=UTF-8",
-            },
-        })
+        return new Response.redirect("https://hsuan.app", 302)
     }
-    const value = await LINKS.get(path)
-    console.log(value)
-
-
-    const location = value
+    const location = await LINKS.get(path)
     if (location) {
         return Response.redirect(location, 302)
-
     }
-    // If request not in kv, return 404
-    return new Response(html404, {
-        headers: {
-            "content-type": "text/html;charset=UTF-8",
-        },
-        status: 404
-    })
+    return new Response.redirect("https://hsuan.app", 302)
 }
 
 
